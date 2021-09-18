7737f5b9c64610f673bbcb4b6d06249705ffc3ba
Szymon Wójcik <cersin96@gmail.com>
2021-09-18 11:09:32 +0200

    .

---
+++ dist/_nuxt/70e2592.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{250:function(e,t,n){"use strict";n.r(t);var r={name:"TheArrow"},c=n(13),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("a",{attrs:{href:"#"}})}),[],!1,null,"26cf3253",null);t.default=component.exports}}]);
